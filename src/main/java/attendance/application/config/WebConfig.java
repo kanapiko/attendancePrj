2d809ea018672d8920a1f1aedcc6d657dd3edeec
seiji <miukanaseiji@gmail.com>
2018-02-19 00:06:59 +0900

    ログインセッションチェック処理を追加

--- src/main/java/attendance/application/config/WebConfig.java
+++ src/main/java/attendance/application/config/WebConfig.java
@@ -1,14 +1,20 @@
 package attendance.application.config;
 
+import attendance.application.interceptor.AdminLoginCheckInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private AdminLoginCheckInterceptor adminLoginCheckInterceptor;
 
     @Bean(name="validator")
     public LocalValidatorFactoryBean localValidatorFactoryBean() {
@@ -28,5 +34,12 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         bean.setBasename("classpath:ValidationMessages");
         bean.setDefaultEncoding("UTF-8");
         return bean;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(adminLoginCheckInterceptor)
+                .addPathPatterns("/admin/**")
+                .excludePathPatterns("/admin/login");
     }
 }
