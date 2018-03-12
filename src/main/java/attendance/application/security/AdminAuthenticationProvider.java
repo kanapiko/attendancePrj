f8772bf7c1fe78a789fae868dbd800249428a170
Seiji Takahashi <s_takahashi@dreamarts.co.jp>
2018-03-12 15:27:10 +0900

    管理者ログイン時のバリデーション修正

--- src/main/java/attendance/application/security/AdminAuthenticationProvider.java
+++ src/main/java/attendance/application/security/AdminAuthenticationProvider.java
@@ -1,11 +1,14 @@
 package attendance.application.security;
 
+import attendance.application.emuns.AuthCd;
+import attendance.application.emuns.Flag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +16,16 @@ import org.springframework.stereotype.Component;
 public class AdminAuthenticationProvider extends DaoAuthenticationProvider {
     @Override
     protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+
         super.additionalAuthenticationChecks(userDetails, authentication);
+
+        AdminUser adminUser = (AdminUser) userDetails;
+
+        // 管理ユーザでない、もしくは削除済み場合は認証失敗
+        if(!adminUser.getUser().authCd.equals(AuthCd.ADMIN.getCode())
+                || adminUser.getUser().delFlg.equals(Flag.ON.getVal())) {
+            throw new UsernameNotFoundException("user not found");
+        }
     }
 
     @Autowired
